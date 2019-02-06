572a1b23d9884366006e6dfb9fc2f0636a2479db
Nicholas Pun <nicholaspun99@gmail.com>
2019-02-05 21:52:58 -0500

    Build basic interface

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
 import './App.css';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      beerData: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://beer-necessities.herokuapp.com/beers')
+      .then((res) => {
+        this.setState({beerData: res.data});
+      })
+  }
+
   render() {
+    const { beerData } = this.state;
+    const { SearchBar } = Search;
+
+    const columns = ['Name', 'Brewery', 'Rating'].map(col => ({ dataField: col, text: col }));
+
+    const expandRow = {
+      renderer: row => (
+        <React.Fragment>
+          <div><b>Look</b>: {row.Description}</div>
+          <div><b>Aroma</b>: {row.Aroma}</div>
+          <div><b>Taste</b>: {row.Taste}</div>
+          <div><b>Finish</b>: {row.Finish}</div>
+        </React.Fragment>
+      )
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ToolkitProvider
+          keyField="Name"
+          data={ beerData }
+          columns={ columns }
+          search
+        >
+          {
+            props => (
+              <div>
+                <h3>Search for beers below: </h3>
+                <SearchBar { ...props.searchProps } />
+                <hr />
+                <BootstrapTable
+                  { ...props.baseProps }
+                  expandRow = { expandRow }
+                />
+              </div>
+            )
+          }
+        </ToolkitProvider>
       </div>
     );
   }
 }
-
-export default App;
