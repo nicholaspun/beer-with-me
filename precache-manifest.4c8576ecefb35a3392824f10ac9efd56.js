86c5a0f617a5f8c02abfe3787cd016e07a37a332
Nicholas Pun <nicholaspun99@gmail.com>
2020-03-14 16:00:40 -0400

    Updates

---
+++ precache-manifest.4c8576ecefb35a3392824f10ac9efd56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d9e141ee5230ffe0193376954472f1",
+    "url": "/beer-with-me/index.html"
+  },
+  {
+    "revision": "b634b6eb56bb9c4d0ddb",
+    "url": "/beer-with-me/static/css/2.978290aa.chunk.css"
+  },
+  {
+    "revision": "ca7175ad37a3343c484c",
+    "url": "/beer-with-me/static/css/main.9d46743b.chunk.css"
+  },
+  {
+    "revision": "b634b6eb56bb9c4d0ddb",
+    "url": "/beer-with-me/static/js/2.3ce17486.chunk.js"
+  },
+  {
+    "revision": "e2915bc6fac5fe5de589e626565a0e7e",
+    "url": "/beer-with-me/static/js/2.3ce17486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7175ad37a3343c484c",
+    "url": "/beer-with-me/static/js/main.b503afd4.chunk.js"
+  },
+  {
+    "revision": "c43730cfa6be2bb55008",
+    "url": "/beer-with-me/static/js/runtime-main.d612a164.js"
+  }
+]);
